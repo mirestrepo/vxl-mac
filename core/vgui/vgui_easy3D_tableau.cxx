0a3f353838500fa0c4812bc435928344735dbd1d
mcgaul <mcgaul@4b17d0e5-ac7a-4109-a8ed-6b3c4e86a14a>
2002-10-02 17:37:45 +0000

    KYM moved vgui_easy3D to vgui_easy3D_tableau
    git-svn-id: https://vxl.svn.sourceforge.net/svnroot/vxl/trunk@6635 4b17d0e5-ac7a-4109-a8ed-6b3c4e86a14a

--- core/vgui/vgui_easy3D_tableau.cxx
+++ core/vgui/vgui_easy3D_tableau.cxx
@@ -1,4 +1,4 @@
-// This is oxl/vgui/vgui_easy3D.cxx
+// This is oxl/vgui/vgui_easy3D_tableau.cxx
 #ifdef VCL_NEEDS_PRAGMA_INTERFACE
 #pragma implementation
 #endif
@@ -6,15 +6,15 @@
 // \file
 // \author Philip C. Pritchett, RRG, University of Oxford
 // \date   24 Sep 99
-// \brief  See vgui_easy3D.h for a description of this file.
+// \brief  See vgui_easy3D_tableau.h for a description of this file.
 
-#include "vgui_easy3D.h"
+#include "vgui_easy3D_tableau.h"
 
 #include "vgui_soview3D.h"
-#include "vgui_displaylist3D.h"
+#include "vgui_displaylist3D_tableau.h"
 #include "vgui_style_factory.h"
 
-vgui_easy3D::vgui_easy3D() {
+vgui_easy3D_tableau::vgui_easy3D_tableau() {
   fg[0] = 0.0f;
   fg[1] = 1.0f;
   fg[2] = 0.0f;
@@ -24,17 +24,17 @@ vgui_easy3D::vgui_easy3D() {
 }
 
 
-void vgui_easy3D::add(vgui_soview3D* object) {
+void vgui_easy3D_tableau::add(vgui_soview3D* object) {
 
   vgui_style *style = vgui_style_factory::instance()->get_style(fg[0], fg[1], fg[2],
                                                                 point_size, line_width);
 
   object->set_style(style);
-  vgui_displaylist3D::add(object);
+  vgui_displaylist3D_tableau::add(object);
 }
 
 
-vgui_point3D* vgui_easy3D::add_point(float x, float y, float z)
+vgui_point3D* vgui_easy3D_tableau::add_point(float x, float y, float z)
 {
   vgui_point3D *obj = new vgui_point3D;
 
@@ -47,7 +47,7 @@ vgui_point3D* vgui_easy3D::add_point(float x, float y, float z)
 }
 
 
-vgui_lineseg3D* vgui_easy3D::add_line(float x0, float y0, float z0,
+vgui_lineseg3D* vgui_easy3D_tableau::add_line(float x0, float y0, float z0,
                                       float x1, float y1, float z1) {
   vgui_lineseg3D *obj = new vgui_lineseg3D;
 
@@ -63,19 +63,19 @@ vgui_lineseg3D* vgui_easy3D::add_line(float x0, float y0, float z0,
 }
 
 
-void vgui_easy3D::set_foreground(float r, float g, float b)
+void vgui_easy3D_tableau::set_foreground(float r, float g, float b)
 {
   fg[0] = r;
   fg[1] = g;
   fg[2] = b;
 }
 
-void vgui_easy3D::set_line_width(float w)
+void vgui_easy3D_tableau::set_line_width(float w)
 {
   line_width = w;
 }
 
-void vgui_easy3D::set_point_radius(float r)
+void vgui_easy3D_tableau::set_point_radius(float r)
 {
   point_size = r;
 }
--- core/vgui/vgui_easy3D_tableau.h
+++ core/vgui/vgui_easy3D_tableau.h
@@ -1,6 +1,6 @@
-// This is oxl/vgui/vgui_easy3D.h
-#ifndef vgui_easy3D_h_
-#define vgui_easy3D_h_
+// This is oxl/vgui/vgui_easy3D_tableau.h
+#ifndef vgui_easy3D_tableau_h_
+#define vgui_easy3D_tableau_h_
 #ifdef VCL_NEEDS_PRAGMA_INTERFACE
 #pragma interface
 #endif
@@ -10,30 +10,31 @@
 // \date   24 Sep 99
 // \brief  Tableau to display three-dimensional geometric objects.
 //
-//  Contains classes: vgui_easy3D  vgui_easy3D_new
+//  Contains classes: vgui_easy3D_tableau  vgui_easy3D_tableau_new
 //
 // \verbatim
 //  Modifications:
-//    24-SEP-1999  P.Pritchett - Initial version.
-//    26-APR-2002  K.Y.McGaul - Added some doxygen-style documentation.
+//   24-SEP-1999 P.Pritchett - Initial version.
+//   26-APR-2002 K.Y.McGaul - Added some doxygen-style documentation.
+//   01-OCT-2002 K.Y.McGaul - Moved vgui_easy3D to vgui_easy3D_tableau. 
 // \endverbatim
 
-#include "vgui_easy3D_sptr.h"
-#include "vgui_displaylist3D.h"
+#include "vgui_easy3D_tableau_sptr.h"
+#include "vgui_displaylist3D_tableau.h"
 class vgui_point3D;
 class vgui_lineseg3D;
 
 //: Tableau to display three-dimensional geometric objects.
 //
 //  Three-dimensional objects (see vgui_soview3D) can be added using add, or add_point, add_line.
-class vgui_easy3D : public vgui_displaylist3D
+class vgui_easy3D_tableau : public vgui_displaylist3D_tableau
 {
  public:
-  //: Constructor - don't use this, use vgui_easy3D_new.
-  vgui_easy3D();
+  //: Constructor - don't use this, use vgui_easy3D_tableau_new.
+  vgui_easy3D_tableau();
 
   //: Returns the tableau name.
-  vcl_string type_name() const { return "vgui_easy3D"; }
+  vcl_string type_name() const { return "vgui_easy3D_tableau"; }
 
   //: Add the given vgui_soview3D to the display.
   void add(vgui_soview3D*);
@@ -58,20 +59,20 @@ class vgui_easy3D : public vgui_displaylist3D
 
 
  protected:
-  ~vgui_easy3D() { }
+  ~vgui_easy3D_tableau() { }
 
   float fg[3];
   float line_width;
   float point_size;
 };
 
-//: Create a smart-pointer to a vgui_easy3D tableau.
-struct vgui_easy3D_new : public vgui_easy3D_sptr
+//: Create a smart-pointer to a vgui_easy3D_tableau tableau.
+struct vgui_easy3D_tableau_new : public vgui_easy3D_tableau_sptr
 {
-  typedef vgui_easy3D_sptr base;
+  typedef vgui_easy3D_tableau_sptr base;
 
-  //: Constructor - create an empty vgui_easy3D.
-  vgui_easy3D_new() : base(new vgui_easy3D()) { }
+  //: Constructor - create an empty vgui_easy3D_tableau.
+  vgui_easy3D_tableau_new() : base(new vgui_easy3D_tableau()) { }
 };
 
-#endif // vgui_easy3D_h_
+#endif // vgui_easy3D_tableau_h_
